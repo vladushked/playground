619993a7b8c29ccd4005281b83ed0ca4d0de2a85
Vladislav Plotnikov <vladik1209@gmail.com>
2021-08-14 22:24:48 +0300

    custom msg and srv

--- ros2tutorials/src/py_pubsub/py_pubsub/publisher_member_function.py
+++ ros2tutorials/src/py_pubsub/py_pubsub/publisher_member_function.py
@@ -1,23 +1,23 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num
 
 
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Num, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Num()
+        msg.num = self.i
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info('Publishing: "%s"' % msg.num)
         self.i += 1
 
 
--- ros2tutorials/src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ ros2tutorials/src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from tutorial_interfaces.msg import Num
 
 
 class MinimalSubscriber(Node):
@@ -9,14 +9,14 @@ class MinimalSubscriber(Node):
     def __init__(self):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
-            String,
+            Num,
             'topic',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg.num)
 
 
 def main(args=None):
